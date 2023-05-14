defc555c7c8cfcf34096b40bdfb5c001f9c25c69
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2023-05-14 00:55:29 -0700

    Updated Main.jsx

--- coursebuilder/src/Components/MAINS/Main.jsx
+++ coursebuilder/src/Components/MAINS/Main.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
-import Header from '../Layout/Header/Header'
+import { useState } from 'react';
+import React from 'react';
+import Header from '../Layout/Header/Header';
+import { Button, Container, HStack, Heading, Input, Text } from '@chakra-ui/react';
+
 
 const Main = () => {
-  return (
-    <>
-    <Header />
-    
-    
-    </>
 
+  const [Keyword, setKeyword] = useState('');
+  const [Category, setCategory] = useState('');
+  const categories = [
+    "Interview Prep",
+    "INTERVIEW DSA PRACTICE",
+    "AWS ASSOCIATES ARCHITECT SAA-C03",
+  ]
+
+  return (
+    <><Header />
     
-  )
-}
+    <Container minH={'95vh'} maxW={"container.lg"} paddingY={'8'}>
+      <Heading children="ALL THE CONTENT" m={'8'} />
+
+      <Input value={Keyword}
+        onChange={e => setKeyword(e.target.value)}
+        placeholder='WHAT ARE YOU LOOKIN FOR?'
+        type={'text'} 
+        focusBorderColor= 'gray.500' />
+
+        <HStack padding={'8'}>
+          {
+            categories.map((item, index)=>(
+              <Button key={index} onClick={() => setCategory(item)} minW={'60'}>
+                <Text children = {item} />
+              </Button>
+            ))
+          }
+        </HStack>
+      
+
+    </Container></>
+    
+  );
+};
 
 export default Main
