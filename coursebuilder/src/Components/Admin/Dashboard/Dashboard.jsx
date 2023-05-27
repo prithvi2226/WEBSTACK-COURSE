aadacf15b95db92f209f371d597d60ecf8775a13
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2023-05-26 23:53:21 -0700

    Updated Dasboard

--- coursebuilder/src/Components/Admin/Dashboard/Dashboard.jsx
+++ coursebuilder/src/Components/Admin/Dashboard/Dashboard.jsx
@@ -132,6 +132,8 @@ const Dashboard = () => {
                         mb={'4'}
                         children="Users" />
 
+                {/* Donut Graph */}
+
             </Box>
 
           </Grid>
