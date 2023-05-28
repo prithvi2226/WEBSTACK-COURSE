ad04aae08c397ceddc0b4e35462ce044c2e81f54
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2023-05-28 16:27:55 -0700

    Create course page done

--- coursebuilder/src/Components/Admin/CreateCourse/CreateCourse.jsx
+++ coursebuilder/src/Components/Admin/CreateCourse/CreateCourse.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../../Layout/Header/Header'
-import { Box, Grid } from '@chakra-ui/react'
+import { Container, Grid, Heading, VStack, Input, Select, Image, Button } from '@chakra-ui/react'
 import Sidebar from '../Sidebar'
 import cursor from '../../../Assets/Images/cursor.png'
+import { fileUploadCss } from '../../Auth/Register'
+
+
+const categories = [
+  "Interview Prep",
+  "INTERVIEW DSA PRACTICE",
+  "AWS ASSOCIATES ARCHITECT SAA-C03",
+]
 
 const CreateCourse = () => {
+
+
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [createdBy, setCreatedBy] = useState("");
+  const [category, setCategory] = useState("");
+  const [image, setImage] = useState("");
+  const [imagePrev, setImagePrev] = useState("");
+
+  const changeImageHandler = (e) => {
+      
+    const file = e.target.files[0];
+
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+
+    reader.onload = () => {
+      setImagePrev(reader.result);
+      setImage(file);
+    }
+
+  }
+
   return (
     <>
     <Header />
@@ -12,9 +43,84 @@ const CreateCourse = () => {
       templateColumns={['1fr', '5fr 1fr']}
       css={{cursor: `url(${cursor}), default`, }}>
       
-      <Box>
+      <Container py={"16"}>
 
-      </Box>
+        <form action="">
+          <Heading textTransform={'uppercase'}
+                    children= "Create Course"
+                    my={"16"}
+                    textAlign={['center', 'left']} />
+
+          <VStack m={"auto"} spacing={"8"}>
+            <Input required
+                          value={title} 
+                          onChange={e => setTitle(e.target.value)} 
+                          placeholder='Title'
+                          type={'text'}
+                          focusBorderColor="purple.300"
+                          />{' '}
+            <Input required
+                          value={description} 
+                          onChange={e => setDescription(e.target.value)} 
+                          placeholder='Description'
+                          type={'text'}
+                          focusBorderColor="purple.300"
+                          />            
+            <Input required
+                          value={createdBy} 
+                          onChange={e => setCreatedBy(e.target.value)} 
+                          placeholder='Creator'
+                          type={'text'}
+                          focusBorderColor="purple.300"
+                          />
+            
+            <Select focusBorderColor='purple.300'
+                    value={category}
+                    onChange={e => setCategory(e.target.value)}>
+              
+              <option value={""}>Category</option>
+              {categories.map(item=>(
+                <option key={item} value={"item"}>
+                  {item}
+                </option>
+              ))}
+            </Select>
+
+            <Input required  
+                  type={'file'}
+                  focusBorderColor="purple.500"
+                  accept="image/*"
+                  css={{
+                    "&::file-selector-button": {
+                      ...fileUploadCss, 
+                      color: "purple",
+                    },
+                  }}
+                  onChange={changeImageHandler}
+
+                      />
+            
+            {imagePrev && (
+              <Image src={imagePrev}
+                      boxSize={"64"}
+                      objectFit={'contain'} />
+            )}
+
+            <Button w={"full"} 
+                    colorScheme='purple'
+                    type='submit'>
+              Create
+            </Button>
+
+
+
+          </VStack>
+          
+        </form>
+        
+
+
+      </Container>
 
       <Sidebar />
 
