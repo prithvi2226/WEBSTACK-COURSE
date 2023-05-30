fb5326d5af29ad188ef5260000f70df29737cae3
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2023-05-30 15:04:05 -0700

    Connected Mongodb and postman getreq

--- course_SERVER/config/database.js
+++ course_SERVER/config/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 
 export const connectDB = async ()=>{
-    const {conection} = await mongoose.connect(process.env.MONGO_URI);
+    const {connection} = await mongoose.connect(process.env.MONGO_URI);
 
-    console.log(`Mongodb connected with ${conection.host}`)
+    console.log(`Mongodb connected with ${connection.host}`)
 }
--- course_SERVER/controllers/courseController.js
+++ course_SERVER/controllers/courseController.js
@@ -3,9 +3,13 @@ import { Course } from "../models/Course.js"
 
 export const getAllCourses = async(req, res, next) => {
 
-    const courses = await Course.find();
-    res.status(200).json({
+    try{
+        const courses = await Course.find();
+        res.status(200).json({
         success: true,
         courses,
-    });
+       });
+    } catch (error) {
+
+    }
 };
