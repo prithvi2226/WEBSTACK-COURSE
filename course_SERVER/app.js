8f536f4ad69c2270c40533b2f5993f9e4ce4e876
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2023-05-30 17:01:34 -0700

    making new course error handler

--- course_SERVER/app.js
+++ course_SERVER/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import {config} from "dotenv"
+import ErrorMiddleware from "./middlewares/Error.js"
+
 
 config({
     path:"./config/config.env"
@@ -16,3 +18,5 @@ app.use("/api/v1", course)
 app.use("/api/v1", user)
 
 export default app;
+
+app.use(ErrorMiddleware);
--- course_SERVER/controllers/courseController.js
+++ course_SERVER/controllers/courseController.js
@@ -1,15 +1,39 @@
-
+import { catchAsyncError } from "../middlewares/catchAsyncError.js"
 import { Course } from "../models/Course.js"    
+import ErrorHandler from "../utils/errorHandler.js"
 
-export const getAllCourses = async(req, res, next) => {
 
-    try{
-        const courses = await Course.find();
-        res.status(200).json({
+export const getAllCourses = catchAsyncError( async(req, res, next) => {
+    const courses = await Course.find();
+    res.status(200).json({
         success: true,
         courses,
-       });
-    } catch (error) {
+    });
+});
 
+export const createCourse = catchAsyncError( async(req, res, next) => {
+
+    const {title, description, category, createdBy} = req.body;
+
+    if(!title || !description || !category || !createdBy){
+        return next(new ErrorHandler("Please Add All The Fields", 400))
     }
-};
+
+    const file = req.file;
+
+    await Course.create({
+        title, 
+        description, 
+        category, 
+        createdBy, 
+        poster: {
+            public_id: "temp",
+            url: "temp",
+        }
+    })
+   
+    res.status(201).json({
+        success: true,
+        message: "Course Created Successfully, U can add Lecture Videos Now!",
+    });
+});
