91b1bdaf0bdb0530321e4330fcc97bb872bfa0b7
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2023-05-31 22:53:03 -0700

    chnages

--- course_SERVER/controllers/userController.js
+++ course_SERVER/controllers/userController.js
@@ -1,44 +1,30 @@
 import { catchAsyncError } from "../middlewares/catchAsyncError.js";
-import ErrorHandler from "../utils/errorHandler.js"
-import { User } from "../models/User.js"
+import ErrorHandler from "../utils/errorHandler.js";
+import { User } from "../models/User.js";
 import { sendToken } from "../utils/sendToken.js";
 
+export const register = catchAsyncError(async (req, res, next) => {
+  const { name, email, password } = req.body;
 
-export const register = catchAsyncError(async(req, res, next)=>{
+  if (!name || !email || !password) {
+    return next(new ErrorHandler("Please Enter All the Fields", 400));
+  }
 
-    const {name, email, password} = req.body;
+  let user = await User.findOne({ email });
 
-    // const file = req.file;
+  if (user) {
+    return next(new ErrorHandler("User Already Exists", 409));
+  }
 
-    if(!name || !email || !password){
-        return next(new ErrorHandler("Please Enter All the Fields", 400));
-    }
+  user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "tempid",
+      url: "tempurl",
+    },
+  });
 
-    let user = await User.findOne({ email });
-
-    if(!user){
-        return next(new ErrorHandler("User Already Exists", 409));
-    }
-
-    //Upload file on cloudinary
-
-    user = await User.create({
-        name,
-        email,
-        password,
-        avatar: {
-            public_id: "tempid",
-            url: "tempurl",
-        }
-    });
-
-    sendToken(res, user, "Registered Successfully", 201);
-
+  sendToken(res, user, "Registered Successfully", 201);
 });
-
-
-
-
-
-
-
--- course_SERVER/models/User.js
+++ course_SERVER/models/User.js
@@ -1,75 +1,87 @@
 import mongoose from "mongoose";
 import validator from "validator";
 import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+
+config();
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
 
 const schema = new mongoose.Schema({
+  // Name type, required
+  name: {
+    type: String,
+    required: [true, "Please Enter Your Name"],
+  },
 
-    //Name type, required
-    name: {
-        type:String,
-        required:[true, "Please Enter Your Name"],
+  // Email
+  email: {
+    type: String,
+    required: [true, "Please Enter Your Email"],
+    unique: true,
+    validate: validator.isEmail,
+  },
+  // Password
+  password: {
+    type: String,
+    required: [true, "Please Enter Your Password"],
+    minLength: [8, "Password Must be at least 8 characters"],
+    select: false,
+  },
+  // Role
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  subscription: {
+    id: String,
+    status: String,
+  },
+  avatar: {
+    public_id: {
+      type: String,
+      required: true,
     },
+    url: {
+      type: String,
+      required: true,
+    },
+  },
 
-    //Email
-    email: {
-        type:String,
-        required:[true, "Please Enter Your Email"],
-        unique: true,
-        validate: validator.isEmail,
+  playlist: [
+    {
+      course: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+      poster: String,
     },
-    //password
-    password: {
-        type:String,
-        required:[true, "Please Enter Your Password"],
-        minLength: [8, "Password Must be atleast 8 characters"],
-        select: false,
-    },
-    //role
-    role: {
-        type:String,
-        enum: ["admin", "user"],
-        default: "user"
-    },
-    subscription: {
-        id: String,
-        status: String,
-    },
-    avatar: {
-        public_id: {
-            type: String,
-            required: true,
-        },
-        url: {
-            type: String,
-            required: true,
-        }
-    },
+  ],
 
-    playlist: [
-        {
-            course: {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: "Course",
-            },
-            poster: String,
-        },
-    ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-
-    ResetPasswordToken: String,
-    ResetPasswordExpire: String,
-
+  ResetPasswordToken: String,
+  ResetPasswordExpire: String,
 });
 
-schema.methods.getJWTToken = function (){
-    return jwt.sign( {_id: this._id}, process.env.JWT_SECRET, {
-        expiresIn: "15d",
-    } );
-}
+schema.methods.getJWTToken = function () {
+  return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
+};
 
 export const User = mongoose.model("User", schema);
-
