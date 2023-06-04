b95a709c5a97c118b9ef162285fdcaf105280637
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2023-06-03 23:33:32 -0700

    lecture delete button started

--- course_SERVER/Routes/courseRoutes.js
+++ course_SERVER/Routes/courseRoutes.js
@@ -1,17 +1,21 @@
 import express from "express"
-import { addLecture, createCourse, getAllCourses, getCourseLecture } from "../controllers/courseController.js";
+import { addLecture, createCourse, deleteCourse, getAllCourses, getCourseLecture } from "../controllers/courseController.js";
 import singleUpload from "../middlewares/multer.js";
+import { authorizeAdmin, isAuthenticated } from "../middlewares/auth.js";
 
 
 const router = express.Router();
 
 //Get all courses without lectures
 router.route("/courses").get(getAllCourses);
+
 //Create new course (ONLY ADMIN)
-router.route("/createcourse").post(singleUpload, createCourse);
+router.route("/createcourse").post(isAuthenticated, authorizeAdmin, singleUpload, createCourse);
 
 // Get course detail
-router.route("/coursedet/:id").get(getCourseLecture).post(singleUpload, addLecture);
+router.route("/coursedet/:id").get(isAuthenticated, getCourseLecture)
+.post(isAuthenticated, authorizeAdmin,  singleUpload, addLecture)
+.delete(isAuthenticated, authorizeAdmin, deleteCourse);
 
 //Delete LectureAdd lecture, Delete Course,
 
--- course_SERVER/controllers/courseController.js
+++ course_SERVER/controllers/courseController.js
@@ -62,24 +62,34 @@ export const getCourseLecture = catchAsyncError( async(req, res, next) => {
     });
 });
 
+//Max Video Size 100MB
 export const addLecture = catchAsyncError( async(req, res, next) => {
-    const course = await Course.findById(id);
-    const{title, description} = req.body;
+    
     const{id} = req.params;
+    const{title, description} = req.body;
 
-    // const file = req.file;
+    const course = await Course.findById(id);
+
 
     if(!course){
         return next(new ErrorHandler("Course Not FounD!", 404));
     }
 
     //upload file here
+    const file = req.file;
+    
+    const fileUri = getDataUri(file);
+
+    const mycloud = await cloudinary.v2.uploader.upload(fileUri.content, {
+        resource_type: "video",
+    });
+
     course.lectures.push({
         title,
         description,
         video: {
-            public_id: "url",
-            url: "url"
+            public_id: mycloud.public_id,
+            url: mycloud.secure_url,
         }
     })
 
@@ -90,6 +100,30 @@ export const addLecture = catchAsyncError( async(req, res, next) => {
     res.status(200).json({
         success: true,
         message: "Lecture Added in Course!",
+    });
+});
+
+
+export const deleteCourse = catchAsyncError( async(req, res, next) => {
+
+    const { id } = req.params;
+
+    const course = await Course.findById(id);
+
+    await cloudinary.v2.uploader.destroy(course.poster.public_id);
+
+    for (let i = 0; i < course.lectures.length; i++) {
+        const singleLecture = course.lectures[i];
+        await cloudinary.v2.uploader.destroy(singleLecture.public_id);
+    }
+    
+    if(!course){
+        return next(new ErrorHandler("Course Not FounD!", 404));
+    }
+   
+    res.status(201).json({
+        success: true,
+        message: "Course deleted Successfully",
     });
 });
 
