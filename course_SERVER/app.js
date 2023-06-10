38f1da979232ae05cd301c0edb8d6cea77938555
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2023-06-09 17:02:59 -0700

    some minor changes

--- course_SERVER/app.js
+++ course_SERVER/app.js
@@ -2,12 +2,15 @@ import express from "express";
 import { config } from "dotenv";
 import ErrorMiddleware from "./middlewares/Error.js";
 import cookieParser from "cookie-parser"; 
+import cors from "cors";
 
 config({
   path: "./config/config.env",
 });
 
 const app = express();
+
+app.get("/", (req, res)=>res.send(`<h1> SITE IS WORKING, CLICK <a href=${process.env.FRONTEND_URL}>HERE </a> TO VIST FRONTEND </h1>`))
 
 // Using middlewares
 app.use(express.json()); // Invoke the express.json() middleware to parse JSON data
@@ -19,6 +22,13 @@ app.use(
 );
 
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"]
+  }
+))
 
 // Importing and using routes
 import course from "./Routes/courseRoutes.js";
