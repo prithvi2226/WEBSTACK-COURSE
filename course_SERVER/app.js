abbed8811da49658c29a9139dc772d8a571ec3ea
Prithvi Prathapan <itsmeprithvi2000@gmail.com>
2023-06-11 02:03:12 -0700

    Commit changes Deployed on heroku WOkring GOOD!

--- course_SERVER/app.js
+++ course_SERVER/app.js
@@ -1,8 +1,12 @@
 import express from "express";
 import { config } from "dotenv";
 import ErrorMiddleware from "./middlewares/Error.js";
-import cookieParser from "cookie-parser"; 
+import cookieParser from "cookie-parser";
 import cors from "cors";
+import course from "./Routes/courseRoutes.js";
+import user from "./Routes/userRoutes.js";
+import payment from "./Routes/paymentRoutes.js";
+import other from "./Routes/otherRoutes.js";
 
 config({
   path: "./config/config.env",
@@ -10,15 +14,12 @@ config({
 
 const app = express();
 
-// Using middlewares
-app.use(express.json()); // Invoke the express.json() middleware to parse JSON data
-
+app.use(express.json());
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
-
 app.use(cookieParser());
 app.use(
   cors({
@@ -28,29 +29,18 @@ app.use(
   })
 );
 
-// Importing and using routes
-import course from "./Routes/courseRoutes.js";
-import user from "./Routes/userRoutes.js";
-import payment from "./Routes/paymentRoutes.js";
-import other from "./Routes/otherRoutes.js";
-
-
-
-
 app.use("/api/v1", course);
 app.use("/api/v1", user);
 app.use("/api/v1", payment);
 app.use("/api/v1", other);
 
-
-app.get("/", (req, res)=>{
-  res.send(`<h1> SITE IS WORKING, CLICK <a href=${process.env.FRONTEND_URL}>HERE </a> TO VIST FRONTEND </h1>`)
-})
-
-export default app;
+app.get("/", (req, res) => {
+  res.send(
+    `<h1> SITE IS WORKING, CLICK <a href=${process.env.FRONTEND_URL}>HERE </a> TO VISIT FRONTEND </h1>`
+  );
+});
 
 app.use(ErrorMiddleware);
 
-
-
+export default app;
 
